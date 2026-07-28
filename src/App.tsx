64cd38bef74b192ade45dbaf90546341ee011f11
Aeryn Elizabeth Lao <aeryn.elizabeth.sy.lao@gmail.com>
2026-07-28 14:02:44 +1200

    chore: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import "./index.css"; 
 
 function App() {
   return (
     <>
       <section id="center">
-        <h1>Go d1 larpers!</h1>
+        <h1 className="text-4xl font-bold text-black underline decoration-pink-500">
+          Go d1 larpers!
+        </h1>
       </section>
     </>
   );
