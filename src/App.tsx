b9835a88e67b082ce61840e7c3e3f1d5f131db31
OliveOfTheBranch <williamdk.101@gmail.com>
2026-08-02 00:57:50 +1200

    feat: read subject from search params

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { use, useState } from "react";
-import bgImg from './assets/Sprite-0003.png';
+import { useMemo, useState } from "react";
+import bgImg from "./assets/Sprite-0003.png";
 import "./index.css";
 
 function App() {
   const [answer, setAnswer] = useState("");
   const [subject, setSubject] = useState("");
+
+  const searchParams = useMemo(() => {
+    return new URLSearchParams(window.location.search);
+  }, [window.location.search]);
+
+  const givenSubject = searchParams.get("subject") || "";
+  if (givenSubject) localStorage.setItem("subject", givenSubject);
 
   const handleChangeAnswer = (event: any) => {
     setAnswer(event.target.value);
@@ -20,13 +27,19 @@ function App() {
 
   return (
     <>
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4 bg-cover bg-center h-screen bg-contain"
-    style={{ backgroundImage: `url(${bgImg})` }}>
-      <div className="w-full max-w-[520px] rounded-2xl bg-white/75 p-6 shadow-xl">
-        <h2 className="text-xl py-[10px] font-bold text-gray-800">Time to Study!</h2>
-        <p className="mt-2 text-gray-600">Answer this question before you can continue scrolling.</p>
+      <div
+        className="flex min-h-screen items-center justify-center bg-gray-100 p-4 bg-cover bg-center h-screen bg-contain"
+        style={{ backgroundImage: `url(${bgImg})` }}
+      >
+        <div className="w-full max-w-[520px] rounded-2xl bg-white/75 p-6 shadow-xl">
+          <h2 className="text-xl py-[10px] font-bold text-gray-800">
+            Time to Study!
+          </h2>
+          <p className="mt-2 text-gray-600">
+            Answer this question before you can continue scrolling.
+          </p>
+        </div>
       </div>
-    </div>
       <section>
         <p>What do you want to learn about?</p>
         <input
@@ -41,7 +54,7 @@ function App() {
       </section>
       <br />
       <section>
-        <p>Explain XYZ to me.</p>
+        <p>Explain {localStorage.getItem("subject")} to me.</p>
         <input
           placeholder="Type your answer here..."
           value={answer}
